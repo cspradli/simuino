921b9085007afc9e784a760e6114b3e7f12156b9
Caleb Spradlin <cspradli@unca.edu>
2020-04-01 17:17:52 -0400

    added updated servuino

--- servuino/sketch.ino
+++ servuino/sketch.ino
@@ -2,7 +2,7 @@
 // SIMUINO_BEGIN
 // ===============================================
 // BOARD_TYPE:  UNO
-// SKETCH_NAME: HelloWorld_UNO.ino
+// SKETCH_NAME: blink.ino
 // SIM_LENGTH:  600
 // WIN_LAYOUT:    2
 // SO_DELAY:     40
@@ -27,12 +27,12 @@
 // Simuino log text customization
 //================================================
 // PINMODE_OUT: 11  "PIN: Led Urgent"
-// PINMODE_OUT: 12  "PIN: Led Blink"
+// PINMODE_OUT: 13  "PIN: Led Blink"
 
 // DIGITALWRITE_LOW:  11  "Waiting"
 // DIGITALWRITE_HIGH: 11  "Urgent"
-// DIGITALWRITE_LOW:  12  "Led is off"
-// DIGITALWRITE_HIGH: 12  "Led is on"
+// DIGITALWRITE_LOW:  13  "Led is off"
+// DIGITALWRITE_HIGH: 13  "Led is on"
 
 // DIGITALREAD:  9  "Read from nine"
 // DIGITALREAD: 10  "Read from ten"
@@ -44,93 +44,24 @@
 // ===============================================
 // SIMUINO_END
 // ===============================================
-//-------- DIGITAL PIN settings ------------------
-#include "EEPROM.h"
-// Leds
-int URGENTLED    = 11;
-int BLINKLED     = 12;
-int IN_PIN       = 10;
-int CONTROL      =  9;
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
  
-//-------- ANALOGUE PIN settings
-int SENSOR1  = 4;
-int SENSOR2  = 5;
+  This example code is in the public domain.
+ */
+int led = 13;
 
-//================================================
-//  Function Declarations
-//================================================
-
-void blinkLed(int n);
-
-//================================================
-void urgent()
-//================================================
-{
-      digitalWriteX(69,URGENTLED, HIGH); 
-      delayX(70,401);
-      digitalWriteX(71,URGENTLED, LOW); 
-}
-//================================================
-void very_urgent()
-//================================================
-{
-      digitalWriteX(77,URGENTLED, HIGH); 
-      delayX(78,402);
-      digitalWriteX(79,URGENTLED, LOW); 
-}
-//================================================
-void setup()
-//================================================
-{
-  Serial.beginX(85,9600); 
-  attachInterruptX(86,0,urgent, CHANGE);
-  attachInterruptX(87,1,very_urgent, RISING);
-  pinModeX(88,BLINKLED,OUTPUT);   
-  pinModeX(89,URGENTLED,OUTPUT);   
-  pinModeX(90,IN_PIN,INPUT);
-  pinModeX(91,CONTROL,INPUT);
-}
-	 
-//================================================ 
-void loop()
-//================================================
-{
-  int value1,value2,i;
-
-  Serial.printlnX(100,"Hello Simuino!");
-  value1 = analogReadX(101,SENSOR1);
-  value2 = analogReadX(102,SENSOR2);
-  Serial.printX(103,"Analog 1 value read: ");
-  Serial.printlnX(104,value1);
-  Serial.printX(105,"Analog 2 value read: ");
-  Serial.printlnX(106,value2);
-  blinkLed(value1);
-  value1 = digitalReadX(108,IN_PIN);
-  value2 = digitalReadX(109,CONTROL);
-  Serial.printX(110,"Digital IN_PIN read: ");
-  Serial.printlnX(111,value1);
-  Serial.printX(112,"Digital CONTROL read: ");
-  Serial.printlnX(113,value2);
-  
-  delayX(115,1000); 
-  EEPROM.writeX(116,55,120);
-  int lue = EEPROM.readX(117,55);
-  Serial.printX(118,"EEPROM value is: ");
-  Serial.printlnX(119,lue);
+// the setup routine runs once when you press reset:
+void setup() {                
+  // initialize the digital pin as an output.
+  pinModeX(58,led, OUTPUT);     
 }
 
-//================================================
-void blinkLed(int n)
-//================================================
-{
-  int i;
-  for(i=1;i<=10;i++)
-    {
-      digitalWriteX(129,BLINKLED, HIGH); 
-      delayX(130,500);
-      digitalWriteX(131,BLINKLED, LOW); 
-    }
+// the loop routine runs over and over again forever:
+void loop() {
+  digitalWriteX(63,led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delayX(64,1000);               // wait for a second
+  digitalWriteX(65,led, LOW);    // turn the LED off by making the voltage LOW
+  delayX(66,1000);               // wait for a second
 }
-//================================================
-// End of Sketch
-//================================================
